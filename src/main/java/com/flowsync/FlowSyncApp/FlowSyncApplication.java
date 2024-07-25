63fbf5a341e89e02361b2ffdadb412d2666288ce
MOHIT SARSAHAY <142138405+mohit07dec@users.noreply.github.com>
2024-07-25 20:34:05 +0530

    Initial setup

---
+++ src/main/java/com/flowsync/FlowSyncApp/FlowSyncApplication.java
@@ -0,0 +1,13 @@
+package com.flowsync.FlowSyncApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowsync/FlowSyncApp/FlowSyncAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowsync.FlowSyncApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowSyncAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
