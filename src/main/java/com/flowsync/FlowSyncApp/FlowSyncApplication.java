53a3c3c83476daa11ef3545686c43901026e1b68
MOHIT SARSAHAY <142138405+mohit07dec@users.noreply.github.com>
2024-07-26 13:17:58 +0530

    Update Service

--- src/main/java/com/flowsync/FlowSyncApp/FlowSyncApplication.java
+++ src/main/java/com/flowsync/FlowSyncApp/FlowSyncApplication.java
@@ -2,8 +2,10 @@ package com.flowsync.FlowSyncApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.flowsync.FlowSyncApp")
 public class FlowSyncApplication {
 
 	public static void main(String[] args) {
